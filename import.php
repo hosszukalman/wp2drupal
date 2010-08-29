4d7532a211e61f4dcb36a4922cfabb5a0f9e140f
Kalman Hosszu <hosszu.kalman@gmail.com>
2010-08-29 17:12:27 +0200

    Add import php base struct: user import.php Class Method and it will runs the command

--- import.php
+++ import.php
@@ -1,7 +1,26 @@
 <?php
+
 ini_set('display_errors', 1);
-error_reporting(E_ALL|E_STRICT);
+error_reporting(E_ALL | E_STRICT);
 
 foreach (glob('lib/*.php') as $filename) {
   include_once $filename;
 }
+
+if ($_SERVER['argc'] !== 3) {
+  throw new Exception('use php import.php [class] [execute|deleteAll]');
+}
+
+$class = $_SERVER['argv'][1];
+if (!class_exists($class)) {
+  throw new Exception($class . ' is not exists!');
+}
+
+$classRef = new ReflectionClass($class);
+$method = $_SERVER['argv'][2];
+if (!$classRef->hasMethod($method)) {
+  throw new Exception($class . '::' . $method . ' is not exists!');
+}
+
+$importer = new $class;
+$importer->$method();
