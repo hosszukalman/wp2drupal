cab78bf373707d860797d393037e149ee732126d
Kalman Hosszu <hosszu.kalman@gmail.com>
2010-08-29 18:08:09 +0200

    Add try block to import.php

--- import.php
+++ import.php
@@ -7,20 +7,24 @@ foreach (glob('lib/*.php') as $filename) {
   include_once $filename;
 }
 
-if ($_SERVER['argc'] !== 3) {
-  throw new Exception('use php import.php [class] [execute|deleteAll]');
-}
+try {
+  if ($_SERVER['argc'] !== 3) {
+    throw new Exception('use php import.php [class] [execute|deleteAll]');
+  }
 
-$class = $_SERVER['argv'][1];
-if (!class_exists($class)) {
-  throw new Exception($class . ' is not exists!');
-}
+  $class = $_SERVER['argv'][1];
+  if (!class_exists($class)) {
+    throw new Exception($class . ' is not exists!');
+  }
 
-$classRef = new ReflectionClass($class);
-$method = $_SERVER['argv'][2];
-if (!$classRef->hasMethod($method)) {
-  throw new Exception($class . '::' . $method . ' is not exists!');
-}
+  $classRef = new ReflectionClass($class);
+  $method = $_SERVER['argv'][2];
+  if (!$classRef->hasMethod($method)) {
+    throw new Exception($class . '::' . $method . ' is not exists!');
+  }
 
-$importer = new $class;
-$importer->$method();
+  $importer = new $class;
+  $importer->$method();
+} catch (Exception $e) {
+  echo $e->getMessage() . PHP_EOL;
+}
