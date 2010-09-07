40967b25ee2c0aa1a8a31d352ae7414ab872bad3
Kalman Hosszu <hosszu.kalman@gmail.com>
2010-09-07 17:06:54 +0200

    Save post main data to Drupal

--- lib/Posts.php
+++ lib/Posts.php
@@ -3,14 +3,41 @@
 class Posts extends Importer {
 
   function __construct() {
-    
+    parent::__construct();
   }
 
   public function deleteAll() {
+    $this->deleteNodes();
+  }
 
+  private function deleteNodes() {
+    $result = db_query("SELECT nid FROM {node} WHERE type = 'blog'");
+
+    while ($row = db_fetch_array($result)) {
+      $this->node_delete($row['nid']);
+    }
   }
 
   public function execute() {
+    $this->savePosts();
+  }
 
+  private function savePosts() {
+    $counter = 0;
+    foreach ($this->dbhWp->query('SELECT p.* FROM wp_posts p WHERE p.post_type = \'post\' ORDER BY p.post_date', PDO::FETCH_ASSOC) as $post) {
+      echo $counter++ . PHP_EOL;
+
+      $node = new stdClass();
+      $node->type = 'blog';
+      $node->uid = 1;
+      $node->created = strtotime($post['post_date']);
+      $node->status = ($post['post_status'] == 'publish');
+      $node->title = $post['post_title'];
+      $node->body = $post['post_content'];
+      $node->format = 3;
+      $node->comment = 2;
+
+      node_save($node);
+    }
   }
 }
